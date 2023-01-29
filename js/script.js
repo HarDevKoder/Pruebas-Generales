bec8f710f897e2db2600ddeeaebbf222bffed243
hardevkoder <haroldvaldes@yahoo.com>
2023-01-29 02:09:43 -0500

    Creacion del Reset y cambio de fuentes y ligaduras

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.onload = clearCache;
+const mensaje =() =>{
+  if (valor === 'old' && valor>=5 || valor != 4){
+    console.log('No pasa nada');
+  }else{
+    console.log('Esperame tantito');
+  }
+}
+
